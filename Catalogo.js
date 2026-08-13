bd87d05c1267773698b1372433074368c619912c
JBLMatadorDePorco <bruno.kelte@escola.pr.gov.br>
2026-08-13 09:17:00 -0300

    as

--- Catalogo.js
+++ Catalogo.js
@@ -1,7 +1,8 @@
 export default class Catalogo { //Cria a classe catalogo e essa classe pode ser exportada por outros arquivos j.s
     #listaDoMidias; //declara uma propriedade e essa propriedade ela é privada ou seja ela só pode ser acessada dentro da classe catalogo
-    constructor() {
-        this.#listaDoMidias = [];
+    constructor() { //é um metodo especial da classe ela executa automaticamente
+        this.#listaDoMidias = [];// this é um objeto atual
+        //criar um array vazio ou seja o catalogo começa sem nenhum elemento 
     }
     adicionar(midia) {
         this.#listaDoMidias.push(midia);
